d294920c28638b4d44a2f319c635d4758cc92c70
nellie <nellie_1996@live.dk>
2019-10-24 12:44:10 +0200

    3. version-fontstyling

--- wp-content/themes/oceanwp-child-grums/functions.php
+++ wp-content/themes/oceanwp-child-grums/functions.php
@@ -1,6 +1,8 @@
 <?php
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
+    
+     wp_enqueue_style('CustomFontJosefineSlab','https://use.typekit.net/yag3jnf.css');
  
     $parent_style = 'parent-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
  
@@ -9,5 +11,9 @@ function my_theme_enqueue_styles() {
         get_stylesheet_directory_uri() . '/style.css',
         array( $parent_style ),
         wp_get_theme()->get('Version')
+        
+        
+        
+        
     );
 }
