a0362bf55ee024a3a6e17a7bee2a6a92c74520cb
Silke Marie  Steen Olesen <silkemariesteenolesen@Silkes-MacBook-Air.local>
2019-10-24 10:49:41 +0200

    2. Version - plugin

--- wp-content/plugins/newsletterplugin/index.php
+++ wp-content/plugins/newsletterplugin/index.php
@@ -17,26 +17,26 @@ function newsletter_form()
     $content .= '<div class="login-form">';
     $content .= '<div class="popupCloseButton">X</div>';
     $content .= '<div class="login-face">';
-    $content .= '<img src=" '.plugins_url("newsletterplugin/img/bog-billede.png").' " ';
+    $content .= '<img src=" '.plugins_url("newsletterplugin/img/logo-tagline-grums-black.png").' " ';
     $content .= 'alt="login-face"></div>';
     $content .= '<div id="promotion-header">';
-    $content .= '<h1 id="promotion-header-title">STRONG AND FIT IN <span>2019</span></h1></div>';
+    $content .= '<h1 id="promotion-header-title">Sign up for Newsletter <span>2019</span></h1></div>';
     $content .= '<section class="form">';
     $content .= '<form action="#">';
     $content .= '<div id="promotion-body">';
-    $content .= '<p id="promotion-body-text">Sign up for our Newsletter and learn to get fit at home with 5 Effective Low-Impact Workouts!</p>';
+    $content .= '<p id="promotion-body-text">Sign up for our Newsletter and become a part of the Grums family. Because we CARE!</p>';
     $content .= '</div>';
     $content .= '<div class="input">';
-    $content .= '<input type="text" id="username" placeholder="John Mckinsey" name="username" required><i class="fa fa-user fa-1x"></i>';
+    $content .= '<input type="text" id="username" placeholder="Name" name="username" required><i class="fa fa-user fa-1x"></i>';
     $content .= '</div>';
     $content .= '<div class="input">';
-    $content .= '<input type="email" id="email" placeholder="mckinsey@hotmail.com" name="email" required><i class="fa fa-envelope fa-1x"></i>';
+    $content .= '<input type="email" id="email" placeholder="E-mail" name="email" required><i class="fa fa-envelope fa-1x"></i>';
     $content .= '</div>';
     $content .= '<div id="submitForm">';
     $content .= '<input type="submit" id="submitBtn" name="submitBtn" value="Subscribe Newsletter!">';
     $content .= '</div>';
     $content .= '<div id="promotion-footer">';
-    $content .= '<p id="promotion-footer-text">Yes, I would like to receive I FORM\'s newsletter with exciting articles as well as marketing about I FORM via e-mail. You can unsubscribe at any time. Read more!</p>';
+    $content .= '<p id="promotion-footer-text">Yes, I would like to receive I GRUMS\'s newsletter via e-mail. You can unsubscribe at any time. Read more!</p>';
     $content .= '</div>';
     $content .= '</form>';
     $content .= '</section>';
@@ -45,11 +45,10 @@ function newsletter_form()
     return $content;
     
 }
-    #First parameter is a self choosen name for a unique short-code. Second parameter is the name of the function that creates the newsletter
+    #This parameter is a self choosen name for a unique short-code. 
     add_shortcode('show_newsletter','newsletter_form');
 
-    # Come back later
-    add_action('wp_enqueue_scripts','register_styles_and_scripts_for_plugin');
+   
 
     function register_styles_and_scripts_for_plugin() 
     {
@@ -70,7 +69,8 @@ function newsletter_form()
         wp_enqueue_script('CustomScript', plugins_url('newsletterplugin/js/script.js'), array('jquery'), null, true); 
     }
 
-
+ #This parameter is the name of the function that creates the newsletter
+    add_action('wp_enqueue_scripts','register_styles_and_scripts_for_plugin');
 
 
 
