9b53fa257507c102ebcd644c20179b0d4603e2fc
nellie <nellie_1996@live.dk>
2019-10-25 13:53:54 +0200

    4. version childtheme

--- wp-content/themes/oceanwp-child-grums/functions.php
+++ wp-content/themes/oceanwp-child-grums/functions.php
@@ -3,17 +3,14 @@ add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
     
      wp_enqueue_style('CustomFontJosefineSlab','https://use.typekit.net/yag3jnf.css');
+     wp_enqueue_style('CustomFontDosis','https://fonts.googleapis.com/css?family=Dosis&display=swap'); 
  
-    $parent_style = 'parent-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
+    $parent_style = 'parent-style'; 
  
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'child-style',
         get_stylesheet_directory_uri() . '/style.css',
         array( $parent_style ),
         wp_get_theme()->get('Version')
-        
-        
-        
-        
     );
 }
